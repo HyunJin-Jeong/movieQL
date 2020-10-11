f032aa89b1a99fdca851cffbf5c4a05c8396298b
hyunjin-jeong <hyun0524e@naver.com>
2020-10-12 00:16:44 +0900

    API 연동 및 limit, rating 기능 추가

--- graphql/db.js
+++ graphql/db.js
@@ -1,56 +1,15 @@
-let movies = [
-    {
-        id: 0,
-        name: "Star Wars - The new one",
-        score: 1
-    },
-    {
-        id: 1,
-        name: "Avengers - The new one",
-        score: 8
-    },
-    {
-        id: 2,
-        name: "The Godfather I",
-        score: 99
-    },
-    {
-        id: 3,
-        name: "Logan",
-        score: 2
+import fetch from "node-fetch";
+const API_URL = "https://yts.am/api/v2/list_movies.json?";
+
+export const getMovies = (limit, rating) => {
+    let REQUEST_URL = API_URL;
+    if (limit > 0) {
+        REQUEST_URL += `limit=${limit}`;
     }
-]
-
-const getMovies = () => movies;
-
-const getById = id => {
-    const filteredMovies = movies.filter(movie => movie.id === id);
-    return filteredMovies[0];
-}
-
-const deleteMovie = id => {
-    const cleanMovies = movies.filter(movie => movie.id !== id);
-    if (movies.length > cleanMovies.length) {
-        movies = cleanMovies;
-        return true;
-    } else {
-        return false;
+    if (rating > 0) {
+        REQUEST_URL += `&minimum_rating=${rating}`;
     }
-}
-
-const addMovie = (name, score) => {
-    const newMovie = {
-        id: movies.length + 1,
-        name,
-        score
-    };
-    movies.push(newMovie);
-    return newMovie;
-}
-
-export {
-    getMovies,
-    getById,
-    deleteMovie,
-    addMovie,
+    return fetch(REQUEST_URL)
+      .then(res => res.json())
+      .then(json => json.data.movies);
 }
